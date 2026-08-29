ff02ed10d3525684a41747904a18d6085f8838b6
Ridho Pujiono <ridhopujiono2@gmail.com>
2026-08-29 21:07:06 +0700

    Add PlanFactory for generating Plan model instances

---
+++ database/factories/PlanFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ridho\JustSubs\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Ridho\JustSubs\Models\Plan;
+use Ridho\JustSubs\Enums\IntervalUnit;
+
+class PlanFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Plan::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->words(2, true),
+            'slug' => $this->faker->unique()->slug(),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->numberBetween(10000, 1000000),
+            'currency' => 'IDR',
+            'interval_count' => 1,
+            'interval_unit' => IntervalUnit::Month,
+            'features' => ['feature1', 'feature2'],
+            'is_active' => true,
+        ];
+    }
+}
