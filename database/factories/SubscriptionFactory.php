73805ae444f888aaf19ab628a9599475fe462f97
Ridho Pujiono <ridhopujiono2@gmail.com>
2026-08-29 21:12:32 +0700

    Add SubscriptionFactory for generating subscription test data

---
+++ database/factories/SubscriptionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ridho\JustSubs\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Ridho\JustSubs\Models\Subscription;
+use Ridho\JustSubs\Models\Plan;
+use Ridho\JustSubs\Enums\SubscriptionStatus;
+
+class SubscriptionFactory extends Factory
+{
+    protected $model = Subscription::class;
+
+    public function definition(): array
+    {
+        return [
+            'plan_id' => Plan::factory(),
+            'subscriber_type' => 'App\Models\User',
+            'subscriber_id' => (string) $this->faker->randomNumber(5),
+            'status' => SubscriptionStatus::Active,
+            'starts_at' => now(),
+            'ends_at' => now()->addMonth(),
+        ];
+    }
+}
