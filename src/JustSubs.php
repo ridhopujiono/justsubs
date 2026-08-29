89db5d98e075e939823958e4d6e5c020d261e7da
Ridho Pujiono <ridhopujiono2@gmail.com>
2026-08-29 21:48:15 +0700

    feat: add subscriber name resolution functionality with customizable callback

--- src/JustSubs.php
+++ src/JustSubs.php
@@ -38,4 +38,43 @@ class JustSubs
 
         return new static;
     }
+
+    /**
+     * The callback that should be used to resolve subscriber display names.
+     *
+     * @var \Closure|null
+     */
+    public static ?Closure $subscriberNameResolver = null;
+
+    /**
+     * Set the callback that should be used to resolve subscriber names.
+     *
+     * @param  \Closure  $callback
+     * @return static
+     */
+    public static function resolveSubscriberNameUsing(Closure $callback)
+    {
+        static::$subscriberNameResolver = $callback;
+
+        return new static;
+    }
+
+    /**
+     * Get the display name for a subscriber.
+     *
+     * @param  mixed  $subscriber
+     * @return string
+     */
+    public static function getSubscriberName($subscriber): string
+    {
+        if (static::$subscriberNameResolver) {
+            return call_user_func(static::$subscriberNameResolver, $subscriber);
+        }
+
+        if (! $subscriber) {
+            return 'Unknown';
+        }
+
+        return $subscriber->name ?? $subscriber->email ?? $subscriber->id ?? 'Unknown';
+    }
 }
