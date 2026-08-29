199abdedcaf92a8dcb6481e998d4f87ef8fbf4b6
Ridho Pujiono <ridhopujiono2@gmail.com>
2026-08-29 21:29:06 +0700

    chore: add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/views/**/*.blade.php",
+    "./src/**/*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
