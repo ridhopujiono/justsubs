c10673842b19c263b19efbdec6bb398c96f342a1
Ridho Pujiono <ridhopujiono2@gmail.com>
2026-08-29 21:13:09 +0700

    Add AlreadySubscribedException for handling active subscription errors

---
+++ src/Exceptions/AlreadySubscribedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ridho\JustSubs\Exceptions;
+
+use Exception;
+
+class AlreadySubscribedException extends Exception
+{
+    public static function forSubscriber($subscriber)
+    {
+        return new self("Subscriber already has an active subscription.");
+    }
+}
